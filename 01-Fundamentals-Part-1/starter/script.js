b085d6533c1ee3cdd7e1b53a59c48c8b408fcd29
Noiddd <dionang11@gmail.com>
2022-03-29 18:19:04 +0800

    Values and Variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,9 @@
 let js = "amazing";
 if (js === "amazing") alert("Javasript is FUN!");
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas"); // Jonas is a value
+console.log(23); // 23 is a value
+
+let firstName = "Jonas"; // firstName is a variable
+console.log(firstName);
