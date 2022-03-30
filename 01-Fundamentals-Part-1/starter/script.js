3d6d704094b7304dd67fe5bdd8d10a0a97b874cb
Noiddd <dionang11@gmail.com>
2022-03-30 17:21:10 +0800

    Data Types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -6,4 +6,18 @@ console.log("Jonas"); // Jonas is a value
 console.log(23); // 23 is a value
 
 let firstName = "Jonas"; // firstName is a variable
-console.log(firstName);
+console.log(firstName); // Prints: "Jonas"
+
+// There are only 2 types of values in Javascript,
+// An Objects
+let me = {
+  name: "Jonas",
+};
+
+// Or a Primitive values
+// There are 7 primitive value types
+// Number, string, boolean, null, undefined, symbols and bigint
+let lasttName = "Ang";
+let age = 30;
+
+console.log(typeof null);
