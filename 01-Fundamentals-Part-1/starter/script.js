9e155bad1af8f6c7b3c5953453ae58d06b1995e9
Dion <dionang11@gmail.com>
2022-03-31 11:16:53 +0800

    Taking Decisions: if / else statements

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -79,7 +79,6 @@ console.log(markBMI, johnBMI);
 
 const markHigherBMI = markBMI > johnBMI;
 console.log(markHigherBMI);
-*/
 
 const firstName = "Jonas";
 const job = "teacher";
@@ -96,3 +95,23 @@ console.log(jonasNew);
 console.log(`Hello world
 this is a multi line
 para`);
+*/
+
+const age = 15;
+
+if (age >= 18) {
+  console.log(`Sarah can start driving!`);
+} else {
+  const yearsLeft = 18 - age;
+  console.log(`Sarah is too young, wait another ${yearsLeft} years`);
+}
+
+let century;
+const birthYear = 2005;
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+
+console.log(century);
