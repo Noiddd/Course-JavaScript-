ebc65ede2b6dc6e1b86edcb5cfe0584cf314ac81
Dion <dionang11@gmail.com>
2022-03-31 10:45:22 +0800

    Operator Precedence

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -21,7 +21,7 @@ let lasttName = "Ang";
 let age = 30;
 
 console.log(typeof null);
-*/
+
 const now = 2037;
 const ageJonas = now - 1991;
 const ageSarah = now - 2018;
@@ -38,3 +38,16 @@ console.log(x);
 
 // Comparison operator
 console.log(ageJonas > ageSarah);
+*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
