39f6e0192b07cc430aefaf719a94c0b81a844c76
Noiddd <dionang11@gmail.com>
2022-04-01 11:21:30 +0800

    Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,2 +1,21 @@
 "use strict";
 // Strict mode prevents us to do certain things and allows some errors to be visible
+
+//////////////////////////////////////////
+// Functions
+
+function logger() {
+  console.log(`My name is Dion`);
+}
+logger(); // Calling, running or invoking a function
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0); // fruitProcessor(5, 0) returns juice, and we capture the value into applejuice
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
