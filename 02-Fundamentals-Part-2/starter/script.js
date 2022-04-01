0ad4bbb2a478ea73c0db43350ef686291fcf8db3
Noiddd <dionang11@gmail.com>
2022-04-01 11:30:23 +0800

    Function Declarations and Expressions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -3,7 +3,7 @@
 
 //////////////////////////////////////////
 // Functions
-
+/*
 function logger() {
   console.log(`My name is Dion`);
 }
@@ -19,3 +19,20 @@ console.log(appleJuice);
 
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
+*/
+//////////////////////////////////////////
+// Function Declarations vs Expressions
+
+// Function Declarations
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+const age1 = calcAge1(1991); // You can call the function before defining it for function declaration but not expression
+console.log(age1);
+
+// Function Expressions
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+const age2 = calcAge2(1991);
+console.log(age2);
