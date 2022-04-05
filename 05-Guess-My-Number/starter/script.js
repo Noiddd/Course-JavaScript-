ca46e5707f74f1bf9b483750cbd19f81451db620
Noiddd <dionang11@gmail.com>
2022-04-05 21:46:45 +0800

    Project #1: Guess my number!

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,7 @@
-'use strict';
+"use strict";
+// Project #1: Guess my number!
+
+// .message for class and #message for id
+// document.querySelector(".message") selects the message element
+// texContent selects the text content
+console.log(document.querySelector(".message").textContent);
