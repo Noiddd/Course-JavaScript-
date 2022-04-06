9b5716994a2b479d68dffa579d56bf0505a706e2
Noiddd <dionang11@gmail.com>
2022-04-06 22:43:58 +0800

    Selecting and Manipulating Elements

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -5,3 +5,15 @@
 // document.querySelector(".message") selects the message element
 // texContent selects the text content
 console.log(document.querySelector(".message").textContent);
+
+///////////////////////////////////////
+// Selecting and Manipulating Elements
+
+document.querySelector(".message").textContent = "Correct Number!"; // Changing text content
+console.log(document.querySelector(".message").textContent); // Print: Correct Number!
+
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 10;
+
+console.log(document.querySelector(".guess").value);
+document.querySelector(".guess").value = 23;
