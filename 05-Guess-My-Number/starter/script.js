ebbdd0d7668329479f42dedabb8144375bf19de9
Noiddd <dionang11@gmail.com>
2022-04-07 10:22:46 +0800

    Handling Click Events

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,6 +1,6 @@
 "use strict";
 // Project #1: Guess my number!
-
+/*
 // .message for class and #message for id
 // document.querySelector(".message") selects the message element
 // texContent selects the text content
@@ -17,3 +17,14 @@ document.querySelector(".score").textContent = 10;
 
 console.log(document.querySelector(".guess").value);
 document.querySelector(".guess").value = 23;
+*/
+///////////////////////////////////////
+// Handling Click Events
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value); // Usually when we get a value from an input field, it will be a string and since we will be comparing it with a random number later on. Need to convert to number to compare with a number
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "No Number!";
+  }
+});
