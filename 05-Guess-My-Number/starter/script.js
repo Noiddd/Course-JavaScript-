c187ae2ba1d165066edb9fe295f2e857ff3924d7
Noiddd <dionang11@gmail.com>
2022-04-07 13:07:30 +0800

    Coding Challenge #1

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -20,10 +20,10 @@ document.querySelector(".guess").value = 23;
 */
 ///////////////////////////////////////
 // Project #1: Guess my number!
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
-document.querySelector(".number").textContent = secretNumber;
+document.querySelector(".number").textContent = "?";
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value); // Usually when we get a value from an input field, it will be a string and since we will be comparing it with a random number later on. Need to convert to number to compare with a number
@@ -35,6 +35,7 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".message").textContent = "CORRECT NUMBER!";
     document.querySelector("body").style.backgroundColor = "#60b347"; // Selecting the body element, does not require the . infront, since it is not a class. The change will be an inline style, meaning it will be in the html sheet. It does not change the css sheet
     document.querySelector(".number").style.width = "30rem"; // The change will be an inline style, meaning it will be in the html sheet. It does not change the css sheet
+    document.querySelector(".number").textContent = secretNumber;
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "Too high!";
@@ -54,4 +55,28 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".score").textContent = 0;
     }
   }
+});
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Implement a game rest functionality, so that the player can make a new guess! Here is how:
+
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the score and secretNumber variables
+3. Restore the initial conditions of the message, number, score and guess input field
+4. Also restore the original background color (#222) and number width (15rem)
+
+GOOD LUCK 😀
+*/
+
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".guess").value = "";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
 });
