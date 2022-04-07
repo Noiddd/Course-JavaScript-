cec36b65d384ba4755e712e781b960b07fe23951
Noiddd <dionang11@gmail.com>
2022-04-07 13:37:07 +0800

    Refactoring Our Code: The DRY Principle

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -23,17 +23,22 @@ document.querySelector(".guess").value = 23;
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
-
 document.querySelector(".number").textContent = "?";
+
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value); // Usually when we get a value from an input field, it will be a string and since we will be comparing it with a random number later on. Need to convert to number to compare with a number
   console.log(guess, typeof guess);
 
   if (!guess) {
-    document.querySelector(".message").textContent = "No Number!";
+    // document.querySelector(".message").textContent = "No Number!";
+    displayMessage("No Number!");
   } else if (guess === secretNumber) {
-    document.querySelector(".message").textContent = "CORRECT NUMBER!";
+    // document.querySelector(".message").textContent = "CORRECT NUMBER!";
+    displayMessage("CORRECT NUMBER!");
     document.querySelector("body").style.backgroundColor = "#60b347"; // Selecting the body element, does not require the . infront, since it is not a class. The change will be an inline style, meaning it will be in the html sheet. It does not change the css sheet
     document.querySelector(".number").style.width = "30rem"; // The change will be an inline style, meaning it will be in the html sheet. It does not change the css sheet
     document.querySelector(".number").textContent = secretNumber;
@@ -41,25 +46,38 @@ document.querySelector(".check").addEventListener("click", function () {
       highscore = score;
       document.querySelector(".highscore").textContent = highscore;
     }
-  } else if (guess > secretNumber) {
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector(".message").textContent = "Too high!";
+      //   document.querySelector(".message").textContent =
+      //     guess > secretNumber ? "Too high!" : "Too low!";
+      displayMessage(guess > secretNumber ? "Too high!" : "Too low!");
       score--;
       document.querySelector(".score").textContent = score;
     } else {
-      document.querySelector(".message").textContent = "YOU LOST THE GAME! T.T";
-      document.querySelector(".score").textContent = 0;
-    }
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector(".message").textContent = "Too low!";
-      score--;
-      document.querySelector(".score").textContent = score;
-    } else {
-      document.querySelector(".message").textContent = "YOU LOST THE GAME! T.T";
+      //   document.querySelector(".message").textContent = "YOU LOST THE GAME! T.T";
+      displayMessage("YOU LOST THE GAME! T.T");
       document.querySelector(".score").textContent = 0;
     }
   }
+  //   else if (guess > secretNumber) {
+  //     if (score > 1) {
+  //       document.querySelector(".message").textContent = "Too high!";
+  //       score--;
+  //       document.querySelector(".score").textContent = score;
+  //     } else {
+  //       document.querySelector(".message").textContent = "YOU LOST THE GAME! T.T";
+  //       document.querySelector(".score").textContent = 0;
+  //     }
+  //   } else if (guess < secretNumber) {
+  //     if (score > 1) {
+  //       document.querySelector(".message").textContent = "Too low!";
+  //       score--;
+  //       document.querySelector(".score").textContent = score;
+  //     } else {
+  //       document.querySelector(".message").textContent = "YOU LOST THE GAME! T.T";
+  //       document.querySelector(".score").textContent = 0;
+  //     }
+  //   }
 });
 ///////////////////////////////////////
 // Coding Challenge #1
@@ -80,7 +98,8 @@ document.querySelector(".again").addEventListener("click", function () {
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   document.querySelector(".number").textContent = "?";
   document.querySelector(".score").textContent = score;
-  document.querySelector(".message").textContent = "Start guessing...";
+  //   document.querySelector(".message").textContent = "Start guessing...";
+  displayMessage("Start guessing...");
   document.querySelector(".guess").value = "";
   document.querySelector("body").style.backgroundColor = "#222";
   document.querySelector(".number").style.width = "15rem";
