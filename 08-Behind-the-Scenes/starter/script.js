a0f280d84fa4995499d12da019ba7496ee1124cf
Noiddd <dionang11@gmail.com>
2022-04-12 10:44:03 +0800

    Primitives vs Objects (Primitive vs Reference Types)

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -104,7 +104,7 @@ const jonas = {
   },
 };
 jonas.calcAge();
-*/
+
 // Arguments keyword
 const addExpr = function (a, b) {
   console.log(arguments);
@@ -119,3 +119,22 @@ var addArrow = (a, b) => {
 };
 
 addArrow(2, 5, 8);
+*/
+////////////////////////////////////////
+// Primitives vs Objects (Primitive vs Reference Types)
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age);
+console.log(oldAge);
+
+const me = {
+  name: "Jonas",
+  age: 30,
+};
+const friend = me;
+
+friend.age = 27;
+
+console.log("Friend:", friend);
+console.log("Me:", me);
