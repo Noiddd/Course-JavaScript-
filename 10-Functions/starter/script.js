d48cf847cbd6ab862551bf0e7968777319658949
Noiddd <dionang11@gmail.com>
2022-04-25 16:19:40 +0800

    Default Parameters

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,27 @@
-'use strict';
+"use strict";
+// Defalut Parameters
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  // Setting default values
+  // numPassengers = numPassengers || 1; // When using ||, whenever there is a falsy value, it will return the first truthy
+  // price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking("LH123");
+createBooking("LH123", 2, 800);
+createBooking("LH123", 2);
+createBooking("LH123", 5);
+createBooking("LH123", undefined, 1000); // Setting default
