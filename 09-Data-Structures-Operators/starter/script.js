df96f709b5c6126bcb28fa5a5e02771f8b23b0c6
Noiddd <dionang11@gmail.com>
2022-04-25 10:40:07 +0800

    Coding Challenge #4

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -836,7 +836,7 @@ const checkBaggage = function (items) {
 checkBaggage("I have a laptop, some Food and a pocket Knife");
 checkBaggage("Socks and camera");
 checkBaggage("Got some snacks and a gun for protection");
-*/
+
 ///////////////////////////////////////
 // Working with Strings - Part 3
 // .split('x') and .join(' ')
@@ -886,3 +886,70 @@ const plancesInLine = function (n) {
   console.log(`There are ${n} planes in line ${":D".repeat(n)}`);
 };
 plancesInLine(5);
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+
+document.body.append(document.createElement("textarea"));
+document.body.append(document.createElement("button"));
+
+document.querySelector("button").addEventListener("click", function (name) {
+  const text = document.querySelector("textarea").value;
+  const textSplit = text.split("\n");
+
+  for (const [i, text] of textSplit.entries()) {
+    const [first, second] = text.toLowerCase().trim().split("_");
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(`${output.padEnd(20)}${"✅".repeat(i + 1)}`);
+  }
+
+  // My version
+  // for (const text of textSplit) {
+  //   const textTrim = text.trim();
+  //   const textLower = textTrim.toLowerCase();
+  //   const [firstText, lastText] = textLower.split("_");
+  //   const textCamel = firstText + lastText[0].toUpperCase() + lastText.slice(1);
+  //   console.log(textCamel);
+  // }
+});
+
+// const camelCase = function (name) {
+//   const nameTrim = name.trim();
+//   const nameLower = nameTrim.toLowerCase();
+//   const [firstName, lastNames] = nameLower.split("_");
+//   const namesCamel =
+//     firstName + lastNames[0].toUpperCase() + lastNames.slice(1);
+//   console.log(namesCamel);
+// };
