1dc1a031a8a8e20a4bd6a214b967fe6a95ebfd96
Noiddd <dionang11@gmail.com>
2022-04-26 10:25:16 +0800

    Functions Accepting Callback Functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -57,3 +57,34 @@ const newPassport = function (person) {
 newPassport(jonas);
 checkIn(flight, jonas);
 */
+/////////////////////////////////////////////////////////////////////
+// Functions Accepting Callback Functions
+
+const oneWord = function (str) {
+  return str.replace(/ /g, "").toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(" ");
+  return [first.toUpperCase(), ...others].join(" ");
+};
+
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transfermed by: ${fn.name}`);
+};
+
+transformer("Javascript is the best!", upperFirstWord);
+
+transformer("Javascript is the best!", oneWord);
+
+// JS uses callback functions all the time
+const hello = function (name) {
+  console.log(`HELLO ${name}`);
+};
+
+document.body.addEventListener("click", hello);
+
+["dion", "jon", "edward"].forEach(hello);
