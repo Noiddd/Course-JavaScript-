8035e5dbbfeed8a61fca3027610b9092e290f6dc
Noiddd <dionang11@gmail.com>
2022-04-28 17:19:05 +0800

    .filter Method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -307,6 +307,27 @@ const movementsDescriptions = movements.map(
 );
 
 console.log(movementsDescriptions);
+
+/////////////////////////////////////////////////
+// .filter Method
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// This method is favourable over the for loop, since it allows chaining
+const deposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+
+console.log(deposits);
+
+// for loop example
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+console.log(depositsFor);
+
+// withdrawals
+const withdrawals = movements.filter(function (mov) {
+  return mov < 0;
+});
+console.log(withdrawals);
 */
 /////////////////////////////////////////////////
-// Computing Username
