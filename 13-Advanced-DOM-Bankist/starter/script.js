464d9f61d9d4c13376155dff2326a2b3eac57375
Dion <dionang11@gmail.com>
2022-05-13 17:46:34 +0800

    Building a Slider Component: Part 1

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -235,6 +235,53 @@ const imgObserver = new IntersectionObserver(loadImg, {
 imgTargets.forEach(function (img) {
   imgObserver.observe(img);
 });
+
+// Slider
+const slides = document.querySelectorAll(".slide");
+const btnLeft = document.querySelector(".slider__btn--left");
+const btnRight = document.querySelector(".slider__btn--right");
+
+let curSlide = 0;
+const maxSlide = slides.length;
+
+// const slider = document.querySelector(".slider");
+// slider.style.transform = "scale(0.2)";
+// slider.style.overflow = "visible";
+
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - curSlide)}%)`)
+  );
+};
+
+goToSlide(0);
+
+// Next slide
+
+const nextSlide = function () {
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+
+  goToSlide(curSlide);
+};
+
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+  goToSlide(curSlide);
+};
+
+btnRight.addEventListener("click", nextSlide);
+btnLeft.addEventListener("click", prevSlide);
+
+// Current slide = 1: -100%, 0%, 100%, 200%
+
 /*
 ////////////////////////////////////////////////////////////////////////////////////////
 // Selecting, Creating and Deleting Elements
