959c48507d8a2850f228a97e57a003b5ae41b5e4
Dion <dionang11@gmail.com>
2022-05-13 12:20:06 +0800

    Lazy Loading Images

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -208,6 +208,33 @@ allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add("section--hidden");
 });
+
+// Lazy Loading Images
+const imgTargets = document.querySelectorAll("img[data-src]"); // selecting elements that contains the property data src
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener("load", function () {
+    entry.target.classList.remove("lazy-img");
+  });
+
+  observer.unobserve(entry.target);
+};
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  treshold: 0,
+  rootMargin: "200px",
+});
+
+imgTargets.forEach(function (img) {
+  imgObserver.observe(img);
+});
 /*
 ////////////////////////////////////////////////////////////////////////////////////////
 // Selecting, Creating and Deleting Elements
