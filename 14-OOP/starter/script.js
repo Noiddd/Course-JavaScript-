b32c6972001798bff5a108541fa6a643e15bcfcc
Noiddd <dionang11@gmail.com>
2022-05-19 22:18:39 +0800

    Chaining Methods

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -689,16 +689,19 @@ class Account {
 
   deposit(val) {
     this.#movements.push(val);
+    return this;
   }
 
   withdraw(val) {
     this.deposit(-val);
+    return this;
   }
 
   requestLoan(val) {
     if (this._approveLoan(val)) {
       this.deposit(val);
       console.log(`Loan approved`);
+      return this;
     }
   }
 
@@ -714,3 +717,8 @@ const acc1 = new Account("Jonas", "EUR", 1111);
 // console.log(acc1.#movements); //Error
 // console.log(acc1.#pin); //Error
 // console.log(acc1.#approveLoan(100)); //Error
+
+// chaining methods
+
+acc1.deposit(300).deposit(500).withdraw(35).requestLoan(25000).withdraw(4000);
+console.log(acc1.getMovements());
