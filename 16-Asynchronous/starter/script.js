157aa1d12e03b322baa5c61e22414a864d089480
Noiddd <dionang11@gmail.com>
2022-05-29 17:49:46 +0800

    Handling Rejected Promises

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -3,8 +3,6 @@
 const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
-///////////////////////////////////////
-
 const renderCountry = function (data, className = "") {
   const html = `
   <article class="country ${className}">
@@ -21,8 +19,13 @@ const renderCountry = function (data, className = "") {
 </article>`;
 
   countriesContainer.insertAdjacentHTML("beforeend", html);
-  countriesContainer.style.opacity = 1;
 };
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText("beforeend", msg);
+};
+///////////////////////////////////////
+
 /*
 const getCountryAndNeighbour = function (country) {
   // AJAX call country 1
@@ -80,7 +83,14 @@ const getCountryData = function (country) {
       return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
     })
     .then((response) => response.json())
-    .then((data) => renderCountry(data, "neighbour"));
+    .then((data) => renderCountry(data, "neighbour"))
+    .catch((err) => {
+      console.log(`${err}`);
+      renderError(`Something went wrong ${err.message}. Try again!`);
+    })
+    .finally(() => (countriesContainer.style.opacity = 1));
 };
 
-getCountryData("usa");
+btn.addEventListener("click", function () {
+  getCountryData("usa");
+});
