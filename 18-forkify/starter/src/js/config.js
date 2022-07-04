5542a20e8f45786ca2d67ab46da1ae5a37bfbce1
Noiddd <dionang11@gmail.com>
2022-07-04 23:50:35 +0800

    Helpers and Configuration Files

---
+++ 18-forkify/starter/src/js/config.js
@@ -0,0 +1,6 @@
+// Contains all the variables that should be constants and should be resuse across the projects
+// Allow ease in configuration of project, by simply changing some of the data in this config file
+
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes";
+
+export const TIMEOUT_SEC = 10;
--- 18-forkify/starter/src/js/model.js
+++ 18-forkify/starter/src/js/model.js
@@ -1,4 +1,6 @@
 // import { async } from "regenerator-runtime";
+import { API_URL } from "./config.js";
+import { getJSON } from "./helpers.js";
 
 export const state = {
   recipe: {},
@@ -7,15 +9,7 @@ export const state = {
 // Responsible for fetching recipe data from forkify API
 export const loadRecipe = async function (id) {
   try {
-    const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-    );
-    const data = await res.json();
-
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-
-    console.log(res);
-    console.log(data);
+    const data = await getJSON(`${API_URL}/${id}`);
 
     const { recipe } = data.data;
     state.recipe = {
@@ -30,6 +24,6 @@ export const loadRecipe = async function (id) {
     };
     console.log(state.recipe);
   } catch (err) {
-    alert(err);
+    console.log(`${err} BOOMBOOM`);
   }
 };
