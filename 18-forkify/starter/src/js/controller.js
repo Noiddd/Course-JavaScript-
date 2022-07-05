14380f4a4d18d1fbfee7bfa19e36626bb71e8c13
Noiddd <dionang11@gmail.com>
2022-07-05 18:26:13 +0800

    Implementing Search Results - Part 2

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -1,11 +1,16 @@
 import * as model from "./model.js";
 import recipeView from "./views/recipeView.js";
 import searchView from "./views/searchView.js";
+import resultsView from "./views/resultsView.js";
 
 import "core-js/stable"; // this is for polyfilling everything else
 import "regenerator-runtime/runtime"; // this is for polyfilling async await
 
 ///////////////////////////////////////
+
+if (module.hot) {
+  module.hot.accept;
+}
 
 const controlRecipes = async function () {
   try {
@@ -26,6 +31,7 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
     // 1. Get search query
     const query = searchView.getQuery();
     if (!query) return;
@@ -34,7 +40,7 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     // 3. Render results
-    console.log(model.state.search.results);
+    resultsView.render(model.state.search.results);
   } catch (err) {
     console.log(err);
   }
