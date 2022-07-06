5f3259f3c03b57f1b35f2525732bead93435b748
Noiddd <dionang11@gmail.com>
2022-07-06 15:20:10 +0800

    Implementing Pagination - Part 1

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -8,9 +8,9 @@ import "regenerator-runtime/runtime"; // this is for polyfilling async await
 
 ///////////////////////////////////////
 
-if (module.hot) {
-  module.hot.accept;
-}
+// if (module.hot) {
+//   module.hot.accept;
+// }
 
 const controlRecipes = async function () {
   try {
@@ -40,7 +40,7 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     // 3. Render results
-    resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage());
   } catch (err) {
     console.log(err);
   }
