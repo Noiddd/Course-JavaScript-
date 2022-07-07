f61eabdec2a0b67b4666e45561c99c63e336a427
Noiddd <dionang11@gmail.com>
2022-07-07 21:21:04 +0800

    Implementing Bookmarks - Part 2

---
+++ 18-forkify/starter/src/js/views/bookmarksView.js
@@ -0,0 +1,18 @@
+import View from "./View.js";
+import previewView from "./previewView.js";
+import icons from "url:../../img/icons.svg";
+
+class BookmarksView extends View {
+  _parentElement = document.querySelector(".bookmarks__list");
+  _errorMessage =
+    "No bookmarks yet... Find an interesting recipe and bookmark it! (>.<)";
+  _message = "";
+
+  _generateMarkup() {
+    return this._data
+      .map((bookmarks) => previewView.render(bookmarks, false))
+      .join("");
+  }
+}
+
+export default new BookmarksView();
