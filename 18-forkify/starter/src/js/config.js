686a0d9992555178fc0af56fd23a38eac599d9b9
Noiddd <dionang11@gmail.com>
2022-07-08 14:10:34 +0800

    Uploading a New Recipe - Part 2

--- 18-forkify/starter/src/js/config.js
+++ 18-forkify/starter/src/js/config.js
@@ -6,3 +6,7 @@ export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
 export const TIMEOUT_SEC = 10;
 
 export const RES_PER_PAGE = 10;
+
+export const KEY = "e2099311-d8b8-49f2-b7ab-f938d4fe1eb4";
+
+export const MODAL_CLOSE_SEC = 2.5;
